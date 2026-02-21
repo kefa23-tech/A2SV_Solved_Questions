1d9a320c6b9eef57a80aaee084431d41b5870778
kefa23-tech <wendimantru@gmail.com>
2026-02-21 23:43:26 +0300

    Create H-index.py

---
+++ H-index.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse = True)
+
+        h = 0
+
+        for i in citations:
+            if i>h:
+                h+=1
+
+        return h
