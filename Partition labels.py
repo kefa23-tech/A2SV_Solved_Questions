e4c470e538f03fbd4bcf269d421f6c748fa2c6ff
kefa23-tech <wendimantru@gmail.com>
2026-02-25 12:53:30 +0300

    Implement partitionLabels method in Solution class

---
+++ Partition labels.py
@@ -0,0 +1,13 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last = {ch: i for i, ch in enumerate(s)}
+        result = []
+        start = end = 0
+
+        for i, ch in enumerate(s):
+            end = max(end, last[ch])
+            if i == end:
+                result.append(end - start + 1)
+                start = i + 1
+        
+        return result
