c397f8dc9eb066ca6903f80d77a7037e3878b06b
kefa23-tech <wendimantru@gmail.com>
2026-03-21 01:09:42 +0300

    Add solution for Same Tree

--- leetcode/0100-same-tree/0100-same-tree.py
+++ leetcode/0100-same-tree/0100-same-tree.py
@@ -6,31 +6,55 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
-        arr1 = []
-        def dfs(Node):
-            nonlocal arr1
-            if not Node:
-                arr1.append("n")
-                return 
-            arr1.append(Node.val)
-            dfs(Node.left)
-            dfs(Node.right)
 
-        arr2 = []
-        def dfs2(Node):
-            nonlocal arr2
+
+
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # arr1 = []
+        # def dfs(Node):
+        #     nonlocal arr1
+        #     if not Node:
+        #         arr1.append("n")
+        #         return 
+        #     arr1.append(Node.val)
+        #     dfs(Node.left)
+        #     dfs(Node.right)
+
+        # arr2 = []
+        # def dfs2(Node):
+        #     nonlocal arr2
   
-            if not Node:
-                arr2.append("n")
-                return 
-            arr2.append(Node.val)
-            dfs2(Node.left)
-            dfs2(Node.right) 
+        #     if not Node:
+        #         arr2.append("n")
+        #         return 
+        #     arr2.append(Node.val)
+        #     dfs2(Node.left)
+        #     dfs2(Node.right) 
 
-        dfs(p)
-        dfs2(q)
+        # dfs(p)
+        # dfs2(q)
       
-        return arr1 == arr2
+        # return arr1 == arr2
 
         
