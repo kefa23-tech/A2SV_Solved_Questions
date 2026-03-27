fc24a4540b12ca11eb34132bb17df1613bd7db19
kefa23-tech <wendimantru@gmail.com>
2026-03-27 21:47:28 +0300

    Add solution for Subsets

--- leetcode/0078-subsets/0078-subsets.py
+++ leetcode/0078-subsets/0078-subsets.py
@@ -8,8 +8,7 @@ class Solution:
         def backtrack(start,sub):
             ans.append(sub[:])
             for i in range(start,end):
-                if i > start and nums[i] == nums[i-1]:
-                    continue
+ 
                 sub.append(nums[i])     
                 backtrack(i+1,sub)
                 sub.pop()
