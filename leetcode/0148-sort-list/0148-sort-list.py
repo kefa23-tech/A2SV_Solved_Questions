a46c07f232b5671e541e3affe76175cd36e6f0e9
kefa23-tech <wendimantru@gmail.com>
2026-04-13 17:21:15 +0300

    Add solution for Sort List

--- leetcode/0148-sort-list/0148-sort-list.py
+++ leetcode/0148-sort-list/0148-sort-list.py
@@ -5,46 +5,18 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #base case
-        if not head or not head.next:
-            return head
+
+        nodes = []
+        cur = head
+        while cur:
+            nodes.append(cur.val)
+            cur = cur.next
+        nodes.sort()
+
+        cur = head
+
+        for val in nodes:
+            cur.val = val
+            cur = cur.next
         
-        l = head
-        mid = self.getMid(head)
-        r = mid.next
-        mid.next = None
-
-        # recursion relation
-
-        left = self.sortList(l)
-        right = self.sortList(r)
-
-        return self.merge(left,right)
-        
-
-    def getMid(self,head):
-        slow,fast = head,head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast= fast.next.next
-        
-        return slow
-
-
-    def merge(self,left_half,right_half):
-
-        dummy = ListNode()
-
-        tail = dummy
-
-        while left_half and right_half:
-
-            if left_half.val < right_half.val:
-                tail.next = left_half
-                left_half = left_half.next
-            else:
-                tail.next = right_half
-                right_half = right_half.next
-            tail = tail.next
-        tail.next = left_half or right_half
-        return dummy.next 
+        return head
