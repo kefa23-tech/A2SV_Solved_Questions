ce24b361d4264a79d4e26cb9b0e2752993fd9a5f
kefa23-tech <wendimantru@gmail.com>
2026-04-29 18:22:00 +0300

    Add solution for Course Schedule

--- leetcode/0207-course-schedule/0207-course-schedule.py
+++ leetcode/0207-course-schedule/0207-course-schedule.py
@@ -1,33 +1,84 @@
 from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
+ 
+    
         def dfs(node):
+
             if node in visited:
                 return False
             if graph[node] == []:
                 return True
-            
             visited.add(node)
-
-            for neighbor in graph[node]:
-                if not dfs(neighbor):
+            for nei in graph[node]:
+                if not dfs(nei):
                     return False
-            
+
             visited.remove(node)
             graph[node] = []
+
             return True
 
+
+
+
         graph = defaultdict(list)
-     
+
         for u,v in prerequisites:
-       
             graph[u].append(v)
         visited = set()
-        
-        for crc in range(numCourses):
-            if not dfs(crc):
+        for node in range(numCourses):
+            if not dfs(node):
                 return False
+
+
+        return True    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # def dfs(node):
+        #     if node in visited:
+        #         return False
+        #     if graph[node] == []:
+        #         return True
+            
+        #     visited.add(node)
+
+        #     for neighbor in graph[node]:
+        #         if not dfs(neighbor):
+        #             return False
+            
+        #     visited.remove(node)
+        #     graph[node] = []
+        #     return True
+
+        # graph = defaultdict(list)
+     
+        # for u,v in prerequisites:
+       
+        #     graph[u].append(v)
+        # visited = set()
         
-        return True
+        # for crc in range(numCourses):
+        #     if not dfs(crc):
+        #         return False
+        
+        # return True
             
